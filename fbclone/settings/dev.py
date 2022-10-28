f03eca8bdf3c6a5b6c8112aeb1d2533b1dba69e2
poonamk3 <poonamk@thoughtwin.com>
2022-10-28 10:31:22 +0530

    add_table

--- fbclone/settings/dev.py
+++ fbclone/settings/dev.py
@@ -4,7 +4,7 @@ DATABASES = {
 
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'newfb',
+        'NAME': 'fbclonedb',
         'USER': 'postgres',
         'PASSWORD': 'psql',
         'HOST': 'localhost',
