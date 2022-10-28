72fb7a51e38ae93af913a92766c0781f98f8e502
poonamk3 <poonamk@thoughtwin.com>
2022-10-28 10:15:01 +0530

    newsetup

---
+++ fbclone/settings/dev.py
@@ -0,0 +1,27 @@
+from . base import *
+
+DATABASES = {
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'newfb',
+        'USER': 'postgres',
+        'PASSWORD': 'psql',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+"""
+
+DATABASES = {
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'daj5avcjro8f4r',
+        'USER': 'rzyyczhbuabcsf',
+        'PASSWORD': '7bdeea9ad2d24a0dfb9ede4c86f56e47fa0cf842e045c75a9c407f995f193cad',
+        'HOST': 'ec2-3-208-79-113.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+"""
